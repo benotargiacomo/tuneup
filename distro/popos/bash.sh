280d99c7b10344a74c2599aa164783ab8a1412e5
benotargiacomo <benotargiacomo@gmail.com>
2025-01-14 19:30:52 -0300

    add inputrc configuration for pop os

--- distro/popos/bash.sh
+++ distro/popos/bash.sh
@@ -1,4 +1,7 @@
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
 cp ~/.local/share/tuneup/configs/bashrc ~/.bashrc
 
+[ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
+cp ~/.local/share/tuneup/configs/inputrc ~/.inputrc
+
 source ~/.bashrc
