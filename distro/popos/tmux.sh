a272e2f06ff4177dd7afb4bb3ec9e4d63bbd6f12
benotargiacomo <benotargiacomo@gmail.com>
2025-01-14 22:59:24 -0300

    fix: apt install conflict

--- distro/popos/tmux.sh
+++ distro/popos/tmux.sh
@@ -1,4 +1,4 @@
-sudo apt install -y tmux >/dev/null 2>&1
+sudo apt-get install -y tmux >/dev/null 2>&1
 
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
 cp ~/.local/share/tuneup/configs/tmux.conf ~/.tmux.conf
