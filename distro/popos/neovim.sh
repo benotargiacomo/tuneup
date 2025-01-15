62abf32f5cc6128613315224790d44f802094f7c
benotargiacomo <benotargiacomo@gmail.com>
2025-01-15 13:08:18 -0300

    add neovim installation script for popos

---
+++ distro/popos/neovim.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y ninja-build gettext cmake curl build-essential
+
+cd /tmp && git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
