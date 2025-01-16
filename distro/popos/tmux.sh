a80f85361fd2554ae54730e00c2374789c67e54a
benotargiacomo <benotargiacomo@gmail.com>
2025-01-16 19:57:09 -0300

    add tmux sessionizer script for popos

--- distro/popos/tmux.sh
+++ distro/popos/tmux.sh
@@ -2,3 +2,9 @@ sudo apt-get install -y tmux >/dev/null 2>&1
 
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
 cp ~/.local/share/tuneup/configs/tmux.conf ~/.tmux.conf
+
+mkdir -p ~/code/personal ~/code/work ~/code/github
+
+[ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
+cp ~/.local/share/tuneup/scripts/tmux-sessionizer ~/.local/bin/sss
+chmod +x ~/.local/bin/sss
