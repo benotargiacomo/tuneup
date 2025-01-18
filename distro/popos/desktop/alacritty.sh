4ffb076037b101b6e86a28df0cf1b6761b225ee6
benotargiacomo <benotargiacomo@gmail.com>
2025-01-18 01:04:22 -0300

    add alacritty installation script for popos

---
+++ distro/popos/desktop/alacritty.sh
@@ -0,0 +1,9 @@
+[ ! -d ~/.config/alacritty ] && mkdir -p ~/.config/alacritty
+
+version=$(alacritty --version | cut -d ' ' -f2 | cut -d '.' -f2)
+
+if [ "$version" -ge "13" ]; then
+  cp ~/.local/share/tuneup/configs/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+else
+  cp ~/.local/share/tuneup/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+fi
