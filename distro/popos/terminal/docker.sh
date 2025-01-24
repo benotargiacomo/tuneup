712c8d24bffb276d6e71c1cdde39dc743815dde0
benotargiacomo <benotargiacomo@gmail.com>
2025-01-24 00:00:38 -0300

    add -y flag to docker installation script for popos

--- distro/popos/terminal/docker.sh
+++ distro/popos/terminal/docker.sh
@@ -1,5 +1,5 @@
 # Add Docker's official GPG key:
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -9,10 +9,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # User group for Docker access
 # TODO: Not working on script
+sudo groupadd docker
 sudo usermod -aG docker ${USER}
