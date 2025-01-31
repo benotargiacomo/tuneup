2f4adb492a52a5a5dcb303c4eb670809265b641c
benotargiacomo <benotargiacomo@gmail.com>
2025-01-30 23:18:31 -0300

    fix groupadd stop installation

--- distro/popos/terminal/docker.sh
+++ distro/popos/terminal/docker.sh
@@ -6,14 +6,17 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" |
-  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+	sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update -y
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # User group for Docker access
-# TODO: Not working on script
-sudo groupadd docker
+if ! getent group docker >/dev/null; then
+	sudo groupadd docker
+else
+	echo "Docker group already exists"
+fi
 sudo usermod -aG docker ${USER}
