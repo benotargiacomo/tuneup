eec08926bdef4b8b05edc0edd95ea098f8c2abb4
benotargiacomo <benotargiacomo@gmail.com>
2025-01-30 23:52:10 -0300

    back to neovim compiler installation

--- distro/popos/terminal/neovim.sh
+++ distro/popos/terminal/neovim.sh
@@ -1,14 +1,15 @@
-# sudo apt-get install -y ninja-build gettext cmake curl build-essential
+sudo apt-get install -y ninja-build gettext cmake curl build-essential
 
-# cd /tmp && git clone https://github.com/neovim/neovim
-# cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
-# cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+cd /tmp && git clone https://github.com/neovim/neovim
+cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
-cd /tmp
-wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
-tar -xf nvim.tar.gz
-sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
-sudo cp -R nvim-linux64/lib /usr/local/
-sudo cp -R nvim-linux64/share /usr/local/
-rm -rf nvim-linux64 nvim.tar.gz
-cd -
+# cd /tmp
+# wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
+# tar -xf nvim.tar.gz
+# sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
+# sudo cp -R nvim-linux64/lib /usr/local/
+# sudo cp -R nvim-linux64/share /usr/local/
+# rm -rf nvim-linux64 nvim.tar.gz
+# cd -
+
