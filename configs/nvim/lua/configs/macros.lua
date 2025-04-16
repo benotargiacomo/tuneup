612efa415d15a1055772af659f20b63f29d660b3
benotargiacomo <benotargiacomo@gmail.com>
2025-04-16 12:59:23 -0300

    fix neovim set register macros

--- configs/nvim/lua/configs/macros.lua
+++ configs/nvim/lua/configs/macros.lua
@@ -1,1 +1,4 @@
-vim.fn.setreg("l", 'yoconsole.log("^[pa", ^[pa)')
+local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+
+vim.fn.setreg("l", 'yoconsole.log("' .. esc .. 'pa", ' .. esc .. "pa);")
+
