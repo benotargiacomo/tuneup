a6f996778b9eee157c9f80edd84d5a81adc6c7a1
benotargiacomo <benotargiacomo@gmail.com>
2025-05-02 21:54:27 -0300

    update alacritty installation script

--- distro/popos/desktop/alacritty.sh
+++ distro/popos/desktop/alacritty.sh
@@ -1,9 +1,13 @@
+sudo apt-get install -y alacritty
+
+sudo update-alternatives --set x-terminal-emulator /usr/bin/alacritty
+
 [ ! -d ~/.config/alacritty ] && mkdir -p ~/.config/alacritty
 
 version=$(alacritty --version | cut -d ' ' -f2 | cut -d '.' -f2)
 
 if [ "$version" -ge "13" ]; then
-  cp ~/.local/share/tuneup/configs/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+    cp ~/.local/share/tuneup/configs/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 else
-  cp ~/.local/share/tuneup/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+    cp ~/.local/share/tuneup/configs/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 fi
