cf58dd4d879938d332ce6db0b900a3d88b1727bc
benotargiacomo <benotargiacomo@gmail.com>
2025-05-02 22:16:27 -0300

    add go latest version and node lts installation

--- distro/popos/terminal/mise.sh
+++ distro/popos/terminal/mise.sh
@@ -4,3 +4,6 @@ wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/
 echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
 sudo apt-get update
 sudo apt-get install -y mise
+
+# mise use -g node@lts
+# mise use -g go@latest
