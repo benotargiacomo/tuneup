879303cbac20b50514c85eaa5d4af2cd625c2fa8
benotargiacomo <benotargiacomo@gmail.com>
2025-05-02 21:57:36 -0300

    update code path

--- distro/popos/terminal/tmux.sh
+++ distro/popos/terminal/tmux.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y tmux >/dev/null 2>&1
 [ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
 cp ~/.local/share/tuneup/configs/tmux.conf ~/.tmux.conf
 
-mkdir -p ~/code/personal ~/code/work ~/code/github
+mkdir -p ~/Code/personal ~/Code/work
 
 [ ! -d ~/.local/bin ] && mkdir -p ~/.local/bin
 cp ~/.local/share/tuneup/scripts/tmux-sessionizer ~/.local/bin/sss
