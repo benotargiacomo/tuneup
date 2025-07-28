334f059a81c54d55d303a98485a6babdd9fa4e08
benotargiacomo <benotargiacomo@gmail.com>
2025-07-28 00:41:57 -0300

    remove starship

--- distro/popos/terminal/starship.sh
+++
@@ -1,4 +0,0 @@
-curl -sS https://starship.rs/install.sh | sh
-
-[ -f ~/.config/starship.toml ] && mv ~/.config/starship.toml ~/.config/starship.toml.bak
-cp ~/.local/share/tuneup/configs/starship.toml ~/.config/starship.toml
